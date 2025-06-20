e34bea188d7ff3be556a7fef144474ac5a5d0f10
Jitesh <jiteshgopale26@gmail.com>
2025-06-20 12:51:40 +0530

    structure change

--- Backend/src/secrets.js
+++ Backend/src/secrets.js
@@ -3,3 +3,4 @@ import dotenv from 'dotenv';
 dotenv.config({path:'.env'}) //initialize
 export const JWT_SECRET = process.env.JWT_SECRET;
 export const PORT = process.env.PORT;;
+export const OPENAI_API_KEY = process.env.OPENAI_API_KEY;
